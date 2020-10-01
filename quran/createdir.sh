5e7a2e27a95e48535da59fb28aaaf89ba1728385
muhammad riansyah <mriansyah93@gmail.com>
2020-10-01 12:51:41 +0700

    init quran and averroes

---
+++ quran/createdir.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+declare -a array=("one" "two" "three")
+
+# get length of an array
+arraylength=${#array[@]}
+
+# use for loop to read all values and indexes
+for (( i=1; i<${arraylength}+1; i++ ));
+do
+  echo $i " / " ${arraylength} " : " ${array[$i-1]}
+done
