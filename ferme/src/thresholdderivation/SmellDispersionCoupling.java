2e251fef83da47cd974f8b51d46e5131a29ff0c1
muhammad riansyah <mriansyah93@gmail.com>
2020-10-09 16:48:03 +0700

    learn ferme threshold

--- ferme/src/thresholdderivation/SmellDispersionCoupling.java
+++ ferme/src/thresholdderivation/SmellDispersionCoupling.java
@@ -1,8 +1,12 @@
 class SmellDispersionCoupling {
 	int CINT;
 	float CDISP;
-	// thpoint & thvalue ?
 	
+	int thValue(){
+	}
+	int thPoint(){
+
+	}
 	float countHarmfulness(){
 		return 0.0;
 	}
---
+++ ferme/src/thresholdderivation/ThPoint.java
@@ -0,0 +1,24 @@
+// keyword thpoint dimana saja?
+// 106 kali muncul
+// muncul pertama kali saat pembahasan 5.2.2 CFT
+// DCO = Dispersion Coupling
+class ThPoint{
+/*
+	ThPoint untuk kasus Common Fraction sama dengan ThValue 
+	why?
+	Because Common Fraction for metric is interval [0,1] SAME with percentile that have interval [0,1] as well
+	akibatnya?
+	then ThPoint == ThValue
+*/
+/*
+	distribusi dari nilai CommonFraction including DCO,TCC,WOC adalah pasti interval [0,1]
+*/
+/*
+	informal requirement tentang level : VERY LOW -LOW- MEAN - HIGH - VERY HIGH
+	VERY LOW & VERY HIGH inilah yg disebut extreme threshold.
+	extreme threshold digunakan untuk harmfulness computation.
+	extreme threshold TIDAK digunakan untuk detection strategy
+*/
+
+
+}
