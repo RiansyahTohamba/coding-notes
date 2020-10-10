90e339841a1c5c2e0d390852740fcafb5cc58f9e
muhammad riansyah <mriansyah93@gmail.com>
2020-10-10 13:01:20 +0700

    init html alves

--- ferme/src/thresholdderivation/BenchmarkBasedTh.java
+++ ferme/src/thresholdderivation/BenchmarkBasedTh.java
@@ -1,3 +1,17 @@
+// 5.2.3
+// The most sensible part of our method is the one for BenchmarkBased Thresholds derivation.
 class BenchmarkBasedTh{
-	
+	ThPoint th1;
+	// most of the metric that used in the ferme thesis is Natural number (e.g. CINT) and Real Number
+	// karena itu, we define a method to derive thresholds, to be used in the code smells detection context, from a dataset of software systems used as benchmark. 
+	// apakah akan berbeda REAL VS NATURAL?
+	// thresholds = deriveThreshold(MetricValue)
+	// benchmark = thresholds
+	// inspirasi nya adalah alves
+	// The starting point of our method is the one proposed by Alves et al. in [3], which follows three core requirements: 
+	// (datadriven, robust and pragmatic)
+	// 
+
 }
+
+// T. L. Alves, C. Ypma, and J. Visser, “Deriving metric thresholds from benchmark data,” in 2010 IEEE International Conference on Software Maintenance, 2010, Timisoara: IEEE, pp. 1–10. doi:10.1109/ICSM.2010.5609747.
